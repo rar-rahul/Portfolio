bbb521bd69130f3290327c0e0aaba0167b5dd1e1
rar <rahulraut430@gmail.com>
2023-08-18 16:31:54 +0530

    setup and chnages

---
+++ src/components/Home/Type.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+const Type = () => {
+    return (
+        <Typewriter
+          options={{
+            strings: [
+              "MERN Stack Developer",
+              "I am a Learner📝 and Enthusiastic to Learn new Tech",
+              "Committed Open Source Contributor",
+              "Effective Problem Solver"
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+          }}
+        />
+      )
+}
+
+export default Type
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "../components/Projects/ProjectCard";
+import Particle from "../components/Particle";
+import pg from "../assets/projects/pg.png";
+
+import kickstart from "../assets/projects/kickstart.png";
+
+const Projects = () => {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          Recent Top <strong className="yellow">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={kickstart}
+              isBlog={false}
+              title="E‑Commerce Website"
+              description="This ecommerce website utilizes a REST API built with Nodejs and Expressjs, allowing for easy CRUD operations on user profiles, products and admin panel. The user interface is designed for efficient product management and includes a complete purchase process. The admin panel, accessible for CRUD on users and products, includes a chart for statistics display. Stripe API is integrated to handle customer payments. The website uses MongoDB  as the database."
+              ghLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pg}
+              isBlog={false}
+              title="Blog Website"
+              description="This blogging application features a RESTful API server built with Nodejs and Expressjs. The data is retrieved from the API server and displayed on a user-friendly interface designed using React, Redux, and styled-components for a seamless experience."
+              ghLink=""
+            />
+          </Col>
+
+        </Row>
+      </Container>
+    </Container>
+  )
+}
+
+export default Projects
